0c2a89e0e87b205ecdf63019ebbd0ee889c7851b
Aastha Mahindra <155626319+aasthamahindra@users.noreply.github.com>
2025-07-16 19:55:59 +0530

    added remaining reducers to store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,9 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import customerTypeReducer from '../features/customerTypeSlice';
+import accountIndustryReducer from '../features/customerTypeSlice';
+import teamReducer from '../features/customerTypeSlice';
+import acvRangeReducer from '../features/customerTypeSlice';
 
 // configure store with a reducer per data slice
 export const store = configureStore({
     reducer: {
-        customerType: customerTypeReducer
+        customerType: customerTypeReducer,
+        accountIndustry: accountIndustryReducer,
+        team: teamReducer,
+        acvRangeReducer: acvRangeReducer,
     },
 });
