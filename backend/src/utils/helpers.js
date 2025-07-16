865ab94d9d8ab16f18702f6b4c4051b5d908bc13
Aastha Mahindra <155626319+aasthamahindra@users.noreply.github.com>
2025-07-16 17:53:46 +0530

    added read json helper

---
+++ backend/src/utils/helpers.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+// helper to read JSON files
+const readJson = (filename) => {
+    const filepath = path.join(__dirname, 'data', filename);
+    const content = fs.readFileSync(filepath, 'utf-8');
+    return JSON.parse(content);
+};
+
+module.exports = { readJson };
